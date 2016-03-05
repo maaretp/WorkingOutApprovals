8e473338914222a0171de9b4ddbb7afdf470d1cb
Maaret Pyhäjärvi <maaret.pyhajarvi@gmail.com>
2016-03-05 16:56:11 +0200

    The XML example

---
+++ WorkingOUtApprovals.Nunit/MiscTestsOnNunit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using ApprovalTests.WinForms;
+using NUnit.Framework;
+
+namespace WorkingOutApprovals.Nunit
+{
+    [TestFixture]
+    [UseReporter(typeof(ClipboardReporter))]
+    public class MiscTestsOnNunit
+    {
+        [Test]
+
+  //      [UseReporter(typeof(DiffReporter))]
+        public void LineEndingsTest()
+        {      
+          Approvals.Verify("Jee" + "\n"+"\t"+"r");         
+        }
+
+        [Test]
+        public void XmlTest()
+        {
+            String xml = @"<config><diagnostics><path>C:\temp</path>
+            <proxy usedefault=""true""/></diagnostics></config>";
+            ApprovalTests.Approvals.VerifyXml(xml);              
+        }
+
+    }
+}
