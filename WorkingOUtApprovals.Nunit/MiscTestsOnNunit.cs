2c746676dcb0fcba0dff117b9b56be2f21df739f
Maaret Pyhäjärvi <maaret.pyhajarvi@gmail.com>
2016-03-10 08:40:41 +0200

    Empty frames for testing

--- WorkingOUtApprovals.Nunit/MiscTestsOnNunit.cs
+++ WorkingOUtApprovals.Nunit/MiscTestsOnNunit.cs
@@ -11,31 +11,14 @@ using NUnit.Framework;
 namespace WorkingOutApprovals.Nunit
 {
     [TestFixture]
-    [UseReporter(typeof(ClipboardReporter))]
+
     public class MiscTestsOnNunit
     {
         [Test]
 
-  //      [UseReporter(typeof(DiffReporter))]
-        public void LineEndingsTest()
-        {      
-          Approvals.Verify("Jee" + "\n"+"\t"+"r");         
-        }
-
-        [Test]
-        public void XmlTest()
+        public void OurFirstTest()
         {
-            String xml = @"<config><diagnostics><path>C:\temp</path>
-            <proxy usedefault=""true""/></diagnostics></config>";
-            ApprovalTests.Approvals.VerifyXml(xml);              
-        }
-
-        [Test]
-        public void RandomTest()
-        {
-            var s = Approvals.GetDefaultNamer();
-            Console.WriteLine("This is it: "+ s);
-            
+  
         }
 
     }
